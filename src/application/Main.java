0958f84ae34a453cd1a7a96b1b39fd7c2e6a4676
Lachezar Shumkov <lachezar.shumkov@uconn.edu>
2015-04-27 03:53:26 +0300

    Fixed starting point and ceiling texture

--- src/application/Main.java
+++ src/application/Main.java
@@ -22,12 +22,7 @@ import javafx.stage.StageStyle;
 
 
 public class Main extends Application {
-	/*
-	 * 
-	 * This is a committed version of my code
-	 * Trying to fix latches error with ANTs
-	 * 
-	 */
+	
 	double anchorY;
 	double anchorX;
 	double anchorAngle;
@@ -37,11 +32,12 @@ public class Main extends Application {
 	
 	double centerX;
 	double centerY;
-	double centerZ;
         
 	Image brick = new Image("/application/Brick.jpg");
 	Image floorP = new Image("/application/Floor.jpg");
 	Image doorPic = new Image("/application/Door.jpg");
+	Image wallPic = new Image("/application/wallpaper.jpg");
+	Image ceilingPic = new Image("/application/ceiling.jpg");
 	
 	@Override
 	public void start(Stage primaryStage) {
@@ -53,7 +49,7 @@ public class Main extends Application {
 			primaryStage.setTitle("3D Environment");
 			primaryStage.setScene(scene);
 			primaryStage.initStyle(StageStyle.UNDECORATED);
-			
+			 
 			//Camera
 			PerspectiveCamera camera = new PerspectiveCamera(true);
 //		    camera.getTransforms().addAll(
@@ -65,7 +61,8 @@ public class Main extends Application {
 			
 		    camera.setNearClip(0.1);
 			camera.setFarClip(2000.0);
-			
+			camera.setTranslateZ(-350);
+			camera.setTranslateX(100);
 			//camera.setFieldOfView(35);
 		    scene.setCamera(camera);
 		    
@@ -76,12 +73,25 @@ public class Main extends Application {
 			Box box4 = new Box(50, 100, 500);
 			Box floor = new Box(230, 1, 500);
 			Box door = new Box (100, 100, 1);
+			Box floor2 = new Box(500, 1, 230);
+			Box backwall = new Box(500, 100, 50);
+			Box floor3 = new Box(230, 1, 500);
+			Box leftwall = new Box(50, 100, 500);
+			Box leftcornerwall = new Box(200, 100, 200);
+			Box leftfrontwall = new Box(230, 100, 100);
+			Box ceiling = new Box(1000,1,1000);
+			Box floor4 = new Box(230, 1, 500);
+			Box rightcornerwall = new Box(200, 100, 200);
+			Box rightfrontwall = new Box(230, 100, 100);
+			Box rightwall = new Box(50, 100, 500);
 			
 			box.setTranslateX(centerX);
 			box.setTranslateY(centerY);
+			box.setTranslateZ(99);
 			
 			box2.setTranslateX(centerX + 200);
 			box2.setTranslateY(centerY);
+			box2.setTranslateZ(99);
 			
 			box3.setTranslateZ(400);
 			box3.setTranslateX(centerX - 30);
@@ -97,6 +107,49 @@ public class Main extends Application {
 			
 			door.setTranslateX(centerX + 100);
 			door.setTranslateY(centerY);
+			door.setTranslateZ(150);
+			
+			floor2.setTranslateX(centerX + 80);
+			floor2.setTranslateY(centerY + 50);
+			floor2.setTranslateZ(750);
+			
+			backwall.setTranslateX(centerX + 80);
+			backwall.setTranslateY(centerY);
+			backwall.setTranslateZ(880);
+			
+			floor3.setTranslateZ(390);
+			floor3.setTranslateY(centerY + 49);
+			floor3.setTranslateX(centerX - 170);
+			
+			leftwall.setTranslateX(centerX-300);
+			leftwall.setTranslateZ(390);
+			leftwall.setTranslateY(centerY);
+			
+			leftcornerwall.setTranslateZ(750);
+			leftcornerwall.setTranslateX(centerX-250);
+			leftcornerwall.setTranslateY(centerY);
+			
+			leftfrontwall.setTranslateZ(180);
+			leftfrontwall.setTranslateX(centerX-170);
+			
+			ceiling.setTranslateY(centerY-50);
+			ceiling.setTranslateX(centerX);
+			ceiling.setTranslateZ(500);
+			
+			floor4.setTranslateY(centerY + 49);
+			floor4.setTranslateZ(390);
+			floor4.setTranslateX(centerX + 340);
+			
+			rightcornerwall.setTranslateZ(750);
+			rightcornerwall.setTranslateX(centerX+425);
+			rightcornerwall.setTranslateY(centerY);
+			
+			rightfrontwall.setTranslateZ(100);
+			rightfrontwall.setTranslateX(centerX+340);
+			
+			rightwall.setTranslateX(centerX+480);
+			rightwall.setTranslateZ(390);
+			rightwall.setTranslateY(centerY);
 			
 			//box.setTranslateZ(100);
 		    
@@ -127,26 +180,9 @@ public class Main extends Application {
 	            	break;
 	            	
 	                case RIGHT: 
-	                	box.getTransforms().add(new Rotate(-5, Rotate.Y_AXIS));
+	                	//box.getTransforms().add(new Rotate(-5, Rotate.Y_AXIS));
         				System.out.println("Key Pressed: " + ke.getCode());
 	            	break;
-	            	// Strafing Left
-	                case Q:
-	                	camera.getTransforms().addAll(
-	                				translate = new Translate(-10, centerY, centerZ)
-	                			);
-	                		System.out.println("Key Presed: " + ke.getCode() + translate.toString());
-	            	break;
-	            	
-	            	// Strafing Right
-	                case E:
-	                	camera.getTransforms().addAll(
-	                				translate = new Translate(10, centerY, centerZ)
-	                			);
-	                		System.out.println("Key Presed: " + ke.getCode() + translate.toString());
-	            	break;
-	            	
-	            	
 	                // Camera rotation around Z Axis
 	                case NUMPAD1:
 	                	rotateZ = new Rotate(-1, Rotate.Z_AXIS);
@@ -155,13 +191,13 @@ public class Main extends Application {
         			break;
         			
 	                case NUMPAD3:
-	                	rotateZ = new Rotate(2, Rotate.Z_AXIS);
+	                	rotateZ = new Rotate(1, Rotate.Z_AXIS);
 	                	camera.getTransforms().add(rotateZ);
         				System.out.println("Key Pressed: " + ke.getCode() + rotateZ);
         			break;
         			// Camera rotation Y Axis
 	                case A:
-	                	rotateY = new Rotate(-2, Rotate.Y_AXIS);
+	                	rotateY = new Rotate(-3, Rotate.Y_AXIS);
 	                	camera.getTransforms().add(rotateY);
         				System.out.println("Key Pressed: " + ke.getCode() + rotateY);
         			break;
@@ -196,6 +232,8 @@ public class Main extends Application {
 			PhongMaterial phong = new PhongMaterial();
 			PhongMaterial floorMaterial = new PhongMaterial();
 			PhongMaterial doorMaterial = new PhongMaterial();
+			PhongMaterial wallMaterial = new PhongMaterial();
+			PhongMaterial ceilingMaterial = new PhongMaterial();
 			//phong.setDiffuseColor(Color.GREEN);
 		    phong.setSpecularColor(Color.WHITE);
 		    phong.setDiffuseMap(brick);
@@ -204,11 +242,27 @@ public class Main extends Application {
 		    
 		    floorMaterial.setDiffuseMap(floorP);
 		    floor.setMaterial(floorMaterial);
+		    floor2.setMaterial(floorMaterial);
+		    floor3.setMaterial(floorMaterial);
+		    floor4.setMaterial(floorMaterial);
 		    
 		    doorMaterial.setDiffuseMap(doorPic);
 		    door.setMaterial(doorMaterial);
 		    
+		    wallMaterial.setDiffuseMap(wallPic);
+		    box3.setMaterial(wallMaterial);
+		    box4.setMaterial(wallMaterial);
+		    backwall.setMaterial(wallMaterial);
+		    leftwall.setMaterial(wallMaterial);
+		    leftcornerwall.setMaterial(wallMaterial);
+		    leftfrontwall.setMaterial(wallMaterial);
+		    rightcornerwall.setMaterial(wallMaterial);
+		    rightfrontwall.setMaterial(wallMaterial);
+		    rightwall.setMaterial(wallMaterial);
 		    
+		    ceilingMaterial.setDiffuseMap(ceilingPic);
+		    ceiling.setMaterial(ceilingMaterial);
+		   
 		    //Light
 //		    PointLight pointLight = new PointLight(Color.WHITE);
 //		    pointLight.setTranslateX(centerX);
@@ -233,7 +287,10 @@ public class Main extends Application {
 		    
 		    
 			//adding stuff to the group
-			group.getChildren().addAll(box, box2, box3, box4, floor, door, camera, ambLightRight);
+			group.getChildren().addAll(box, box2, box3, box4,
+					backwall, leftwall, rightwall, leftcornerwall, rightcornerwall,
+					leftfrontwall, rightfrontwall, ceiling,
+					floor, floor2, floor3, floor4, door, camera, ambLightRight);
 			
 		
 			
